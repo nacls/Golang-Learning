300f89818af0b329fe410bd5319da07a1e19c12c
Maryami <mmasoumi15@gmail.com>
2024-05-09 11:21:46 +0330

    read input in tax.go

--- tax.go
+++ tax.go
@@ -1,5 +1,36 @@
 package main
 
-func calculateTax() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func calculateTax() {
+	// Read input
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	line := scanner.Text()
+
+	// Tokenize input
+	words := strings.Fields(line)
+
+	// Check input size
+	if len(words) != 1 {
+		return
+	}
+
+	// Turn string to int
+	income, err := strconv.Atoi(words[0])
+	if err != nil {
+		return
+	}
+
+	if income < 1000 || income > 100000000 {
+		return
+	}
+
+	fmt.Println("Input in correct format")
 }
