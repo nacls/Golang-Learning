aa9c05129175fa173dc38c371c347d8803097d67
Maryami <mmasoumi15@gmail.com>
2024-05-12 16:59:10 +0330

    add clock.go file

---
+++ clock.go
@@ -0,0 +1,11 @@
+package main
+
+func ConvertToDigitalFormat(hour, minute, second int) string {
+	// TODO
+	return ""
+}
+
+func ExtractTimeUnits(seconds int) (int, int, int) {
+	// TODO
+	return 0, 0, 0
+}
