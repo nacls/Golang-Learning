4abd76d70faebcb73217a7c460a6a15ab46cddb8
Maryami <mmasoumi15@gmail.com>
2024-05-12 15:02:55 +0330

    complete the logic for spaceship exercise

--- spaceship.go
+++ spaceship.go
@@ -9,8 +9,48 @@ import (
 )
 
 func getUsagePatternsOfSpaceships() {
-	spaceship, usages := readInputPatterns()
-	fmt.Println(spaceship, usages)
+	usages, spaceship := readInputPatterns()
+
+	for i := 0; i < len(spaceship); i++ {
+		patternNum := calculatePatternNum(usages[i])
+		fmt.Printf("%s %v\n", spaceship[i], patternNum)
+	}
+}
+
+func calculatePatternNum(usageForSpaceship []int) int {
+	if len(usageForSpaceship) < 3 {
+		return 0
+	}
+	numOfSequences := 0
+
+	for i := 0; i < len(usageForSpaceship); i++ {
+		for j := len(usageForSpaceship); j >= 0; j-- {
+			if i <= j {
+				if doesPatternExist(usageForSpaceship[i:j]) {
+					numOfSequences++
+				}
+			}
+		}
+	}
+
+	return numOfSequences
+}
+
+func doesPatternExist(sequence []int) bool {
+	if len(sequence) < 3 {
+		return false
+	}
+	difference := -1
+	for i := 0; i < len(sequence); i++ {
+		if i == 1 {
+			difference = sequence[i] - sequence[i-1]
+		} else if i > 1 {
+			if sequence[i]-sequence[i-1] != difference {
+				return false
+			}
+		}
+	}
+	return true
 }
 
 func readInputPatterns() ([][]int, []string) {
