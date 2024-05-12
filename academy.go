b0b62f826a8f892324cd53276c12292a29bc9e3e
Maryami <mmasoumi15@gmail.com>
2024-05-12 13:45:22 +0330

    read input for academy exercise

--- academy.go
+++ academy.go
@@ -1,5 +1,58 @@
 package main
 
-func calculateTeachersScores() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func calculateTeachersScores() {
+	scores, teacherNames := readInput()
+
+	fmt.Printf("printing input: %v, %v", scores, teacherNames)
+}
+
+func convertStringArrToIntArr(stringArray []string) []int {
+	var intArray []int
+	for _, str := range stringArray {
+		num, err := strconv.Atoi(str)
+		if err != nil {
+			fmt.Printf("Error converting %s to integer: %v\n", str, err)
+			return nil
+		}
+		intArray = append(intArray, num)
+	}
+	return intArray
+}
+
+func readInput() ([][]int, []string) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// number of teachers
+	scanner.Scan()
+	line := scanner.Text()
+
+	// convert string to int
+	numOfTeachers, _ := strconv.Atoi(line)
+
+	var input [][]int
+	var teacherNames []string
+
+	i := 0
+	for i < numOfTeachers*2 {
+		scanner.Scan()
+		line := scanner.Text()
+
+		if i%2 == 0 { // is writing teacher's name
+			teacherNames = append(teacherNames, line)
+		} else { // is a line of student scores
+			inputScoreLine := strings.Fields(line)
+			input = append(input, convertStringArrToIntArr(inputScoreLine))
+		}
+		i++
+	}
+
+	return input, teacherNames
 }
