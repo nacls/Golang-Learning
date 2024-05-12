923fbbdf5e2d2dac6caaa531ab8e66927ee533a6
Maryami <mmasoumi15@gmail.com>
2024-05-12 16:02:59 +0330

    complete the logic for agency exercise

--- agency.go
+++ agency.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func printCountryCodes() {
+	scanner := bufio.NewScanner(os.Stdin)
+	var countryCodes = make(map[string]string)
+
+	scanner.Scan()
+	numOfCountries, _ := strconv.Atoi(scanner.Text())
+
+	for i := 0; i < numOfCountries; i++ {
+		scanner.Scan()
+		countryAndCode := strings.Split(scanner.Text(), " ")
+		countryCodes[countryAndCode[1]] = countryAndCode[0]
+	}
+
+	scanner.Scan()
+	numOfNumbers, _ := strconv.Atoi(scanner.Text())
+
+	for i := 0; i < numOfNumbers; i++ {
+		scanner.Scan()
+		number := scanner.Text()
+		found := false
+		for key, value := range countryCodes {
+			if strings.HasPrefix(number, key) {
+				fmt.Println(value)
+				found = true
+			}
+		}
+		if !found {
+			fmt.Println("Invalid Number")
+		}
+	}
+}
