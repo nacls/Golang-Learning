657a6fecfdefb4b015bddbf2eb520f31ee1d43ce
Maryami <mmasoumi15@gmail.com>
2024-05-12 18:12:42 +0330

    add logic for digital clock exercise

--- clock.go
+++ clock.go
@@ -1,11 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func ConvertToDigitalFormat(hour, minute, second int) string {
-	// TODO
-	return ""
+	return fmt.Sprintf("%s:%s:%s", addZero(hour), addZero(minute), addZero(second))
+}
+
+func addZero(num int) string {
+	if num < 10 {
+		return "0" + strconv.Itoa(num)
+	}
+	return strconv.Itoa(num)
 }
 
 func ExtractTimeUnits(seconds int) (int, int, int) {
-	// TODO
-	return 0, 0, 0
+	hours := seconds / 3600
+	seconds = seconds - hours*3600
+	minutes := seconds / 60
+	seconds = seconds - minutes*60
+	return hours, minutes, seconds
 }
