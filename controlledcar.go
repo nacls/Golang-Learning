b917e6445b6d2d0fd1042288539fdc66735a7d6d
Maryami <mmasoumi15@gmail.com>
2024-05-16 18:53:06 +0330

    complete the logic for controlled car exercise

--- controlledcar.go
+++ controlledcar.go
@@ -1,25 +1,34 @@
 package main
 
+import "fmt"
+
 type Car struct {
-	// TODO
+	speed   int
+	battery int
 }
 
 func NewCar(speed, battery int) *Car {
-	// TODO
-	return nil
+	return &Car{speed: speed, battery: battery}
 }
 func GetSpeed(car *Car) int {
-	// TODO
-	return 0
+	return car.speed
 }
 func GetBattery(car *Car) int {
-	// TODO
-	return 0
+	return car.battery
 }
 func ChargeCar(car *Car, minutes int) {
-	// TODO
+	car.battery += minutes / 2
+	if car.battery > 100 {
+		car.battery = 100
+	}
 }
 func TryFinish(car *Car, distance int) string {
-	// TODO
-	return ""
+	batteryUsage := distance / 2
+	if batteryUsage > car.battery {
+		car.battery = 0
+		return ""
+	}
+	car.battery = car.battery - batteryUsage
+	time := float32(distance) / float32(car.speed)
+	return fmt.Sprintf("%.2f", time)
 }
