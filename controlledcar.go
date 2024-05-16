13ec5bd8d7a881f2f2798c3f4fdf97bdbcb25f3c
Maryami <mmasoumi15@gmail.com>
2024-05-16 16:00:01 +0330

    add TODOs for controlledcar.go

--- controlledcar.go
+++ controlledcar.go
@@ -1,1 +1,25 @@
 package main
+
+type Car struct {
+	// TODO
+}
+
+func NewCar(speed, battery int) *Car {
+	// TODO
+	return nil
+}
+func GetSpeed(car *Car) int {
+	// TODO
+	return 0
+}
+func GetBattery(car *Car) int {
+	// TODO
+	return 0
+}
+func ChargeCar(car *Car, minutes int) {
+	// TODO
+}
+func TryFinish(car *Car, distance int) string {
+	// TODO
+	return ""
+}
