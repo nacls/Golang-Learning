5f99cada43f9863a9bdfec6f96746ba25a8295bc
Maryami <mmasoumi15@gmail.com>
2024-05-21 18:05:33 +0330

    add test logic in main for game exercise

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"Goland-Learning/concurrency/gameserver"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	fmt.Println("test 1:")
+	test1()
+	fmt.Println("------------")
+
+	fmt.Println("test 2:")
+	test2()
+	fmt.Println("------------")
+
+	fmt.Println("test 3:")
+	test3()
+	fmt.Println("------------")
+}
+
+func test1() {
+	g, err := gameserver.NewGame([]int{})
+	fmt.Println(err == nil)
+	fmt.Println(g != nil)
+}
+
+func test2() {
+	g, err := gameserver.NewGame([]int{1, 2, 3})
+	fmt.Println(err == nil)
+
+	err = g.ConnectPlayer("Cyn")
+	fmt.Println(err == nil)
+}
+
+func test3() {
+	g, err := gameserver.NewGame([]int{1, 2, 3})
+	fmt.Println(err == nil)
+
+	err = g.ConnectPlayer("Cyn")
+	fmt.Println(err == nil)
+
+	p, err := g.GetPlayer("CyN")
+	fmt.Println(err == nil)
+	fmt.Println(p != nil)
 }
