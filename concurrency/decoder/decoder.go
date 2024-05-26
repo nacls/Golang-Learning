79793129d1b51d0e0fa2a3170f287e3dbca8a4f4
Maryami <mmasoumi15@gmail.com>
2024-05-26 15:23:06 +0330

    finish decoder exercise

--- concurrency/decoder/decoder.go
+++ concurrency/decoder/decoder.go
@@ -1,1 +1,15 @@
 package decoder
+
+func StartDecipher(senderChan chan string, decipherer func(encrypted string) string) chan string {
+	outputChan := make(chan string, 5)
+
+	go func() {
+		for {
+			encryptedData := <-senderChan
+			decryptedData := decipherer(encryptedData)
+			outputChan <- decryptedData
+		}
+	}()
+
+	return outputChan
+}
